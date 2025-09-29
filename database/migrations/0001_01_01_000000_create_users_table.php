18cfcfa430a675039cff4f8696a3fe65b866eae2
Aldobrass <albertcornelius47@gmail.com>
2025-09-29 14:29:36 +0700

    bagian migrationnya belum update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
+            $table->enum('role', ['marketing', 'ppic', 'keprod', 'qc', 'purchasing', 'gudang', 'packing', 'direktur', 'supplier', 'supir']);
+            $table->enum('work_status', ['dalam pengerjaan', 'tersedia']);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
